a690cbfd79222d4bd0958d9e3434b0887cc6c198
Farya <farya.hsn@gmail.com>
2021-10-08 12:27:52 +0100

    added a message form

--- client/src/components/user-profile/user-profile.js
+++ client/src/components/user-profile/user-profile.js
@@ -1,68 +1,98 @@
-export default class UserProfile extends Component {
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class SendGift extends Component {
   constructor(props) {
-      super(props);
-      this.addActiveClass= this.addActiveClass.bind(this);
-      this.state = {
-          active: false,
-      };
-  this.state = {
-    textarea: ''
-  };
+    super(props);
+
+    this.onChangeUsername = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangeMessage.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
   }
 
-
-  toggleClass() {
-      const currentState = this.state.active;
-      this.setState({ active: !currentState });
-  };
-
-  onTextarea(e) {
+  onChangeName(e) {
     this.setState({
-      textarea: e.target.value
-    })
+      username: e.target.value,
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangeMessage(e) {
+    this.setState({
+      password: e.target.value,
+    });
   }
 
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
-      textarea: this.state.textarea
+    const sendGift = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message,
     };
 
-    console.log(user);
+    console.log(sendGift);
 
     axios
-      .post('http://localhost:5000/users/add', user)
+      .post("http://localhost:5000/profile/username", sendGift)
       .then((res) => console.log(res.data));
     // window.location = '/';
     this.setState({
-      textarea: ''
+      name: "",
+      email: "",
+      message: "",
     });
   }
 
   render() {
-      return (
-          <div 
-              className={this.state.active ? 'send_gift_form': null} 
-              onClick={this.toggleClass} 
-          >
-              <p>{this.props.text}</p>
+    return (
+      <div>
+        <h3>Send Gift</h3>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <input
+              type="text"
+              value={this.state.Name}
+              onChange={this.onChangeName}
+              placeholder="Name"
+            />
           </div>
-      )
-}
-}
-
-class Test extends Component {
-  render() {
-      return (
-        <div>  
-          <button onClick={clickMe}>
-              Buy me beer
-          </button>
-            <form onSubmit={this.submit()}>
-              <input />
-            </form>
+          <div>
+            <input
+              type="email"
+              required
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+              placeholder="Email"
+            />
           </div>
-      );
+          <div>
+            <input
+              className="Message"
+              type="message"
+              value={this.state.message}
+              onChange={this.onChangeMessage}
+              placeholder="Enter message here"
+            />
+          </div>
+          <div>
+            <input type="submit" value="Send Gift" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
