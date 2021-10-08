1de285f7b1710b24b9a668a2ff0c6d8f2d2cadf9
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-08 11:54:18 +0100

    HE - Hand over unfinished form

--- client/src/components/user-profile/user-profile.js
+++ client/src/components/user-profile/user-profile.js
@@ -1,17 +1,68 @@
-function clickMe(){
-  alert('Thanks, you are so generous!')
+export default class UserProfile extends Component {
+  constructor(props) {
+      super(props);
+      this.addActiveClass= this.addActiveClass.bind(this);
+      this.state = {
+          active: false,
+      };
+  this.state = {
+    textarea: ''
+  };
+  }
+
+
+  toggleClass() {
+      const currentState = this.state.active;
+      this.setState({ active: !currentState });
+  };
+
+  onTextarea(e) {
+    this.setState({
+      textarea: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      textarea: this.state.textarea
+    };
+
+    console.log(user);
+
+    axios
+      .post('http://localhost:5000/users/add', user)
+      .then((res) => console.log(res.data));
+    // window.location = '/';
+    this.setState({
+      textarea: ''
+    });
+  }
+
+  render() {
+      return (
+          <div 
+              className={this.state.active ? 'send_gift_form': null} 
+              onClick={this.toggleClass} 
+          >
+              <p>{this.props.text}</p>
+          </div>
+      )
+}
 }
 
-function DonateButton() {
-  return (
-    <div>  
-       <button onClick={clickMe}>
-          Buy me beer
-      </button>
-
-    </div>
-     
-  );
+class Test extends Component {
+  render() {
+      return (
+        <div>  
+          <button onClick={clickMe}>
+              Buy me beer
+          </button>
+            <form onSubmit={this.submit()}>
+              <input />
+            </form>
+          </div>
+      );
+  }
 }
-
-export default DonateButton;
