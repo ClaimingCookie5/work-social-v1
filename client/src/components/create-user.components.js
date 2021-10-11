cb792b283c508741b76c37a92dba884aa058f133
Corinne <corinne.boesch@gmail.com>
2021-10-11 12:41:18 +0100

    changes

--- client/src/components/create-user.components.js
+++ client/src/components/create-user.components.js
@@ -47,7 +47,7 @@ export default class CreateUser extends Component {
     console.log(user);
 
     // .post('http://localhost:5000/users/add', user)
-    axios.post('users/add', user).then((res) => console.log(res.data));
+    axios.post('/users/add', user).then((res) => console.log(res.data));
     // window.location = '/';
     this.setState({
       username: '',
